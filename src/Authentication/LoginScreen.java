3df8932e3214302ab3f571761d8c8bc4eaf91c28
Danyon <matchstick_dan@hotmail.com>
2016-04-27 13:22:36 +1000

    added username field to jpanel to fix not showing issue...
    Also added the actions to the jListener to reset all fields when reset button is pressed

--- src/Authentication/LoginScreen.java
+++ src/Authentication/LoginScreen.java
@@ -4,66 +4,63 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-class LoginScreen extends JFrame implements ActionListener
-{
-  JButton btnLogin, btnReset, btnExit;
-  JTextField jtfUser;
-  JPasswordField jpfPass;
-  JLabel jlabUser, jlabPass;
+class LoginScreen extends JFrame implements ActionListener {
+    JButton btnLogin, btnReset, btnExit;
+    JTextField jtfUser;
+    JPasswordField jpfPass;
+    JLabel jlabUser, jlabPass;
 
-    public LoginScreen(String name)
-    {
-      super(name);
-      btnLogin = new JButton("Login");
-      btnReset = new JButton("Reset");
-      btnExit = new JButton("Exit");
+    public LoginScreen(String name) {
+        super(name);
 
-      jtfUser = new JTextField();
-      jpfPass = new JPasswordField();
+        JFrame loginScreen = new JFrame();  //creates the frame and sets properties
+        loginScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginScreen.setSize(500, 300);
+        loginScreen.setVisible(true);
 
-      jlabUser = new JLabel("Username:");
-      jlabPass = new JLabel("Password:");
+        jtfUser = new JTextField("Please Enter Username"); //Creates an instance of all elements
+        jpfPass = new JPasswordField();
+        jlabUser = new JLabel("Username:");
+        jlabPass = new JLabel("Password:");
+        btnLogin = new JButton("Login");
+        btnReset = new JButton("Reset");
+        btnExit = new JButton("Exit");
 
-      this.setLayout(null);
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginScreen.add(jlabUser); //Adds all instances to the JFrame
+        loginScreen.add(jlabPass);
+        loginScreen.add(jpfPass);
+        loginScreen.add(jtfUser);
+        loginScreen.add(btnLogin);
+        loginScreen.add(btnReset);
+        loginScreen.add(btnExit);
 
-      btnLogin.addActionListener(this);
-      btnReset.addActionListener(this);
-      btnExit.addActionListener(this);
+        btnLogin.addActionListener(this); //Adds listeners on all buttons
+        btnReset.addActionListener(this);
+        btnExit.addActionListener(this);
 
-      jlabUser.setBounds(10,10,120,20);
-      jlabPass.setBounds(10,30,120,20);
+        jlabUser.setBounds(10, 10, 120, 20); //Adds positioning and sizes to all elements
+        jlabPass.setBounds(10, 30, 120, 20);
 
-      jtfUser.setBounds(140,10,300,20);
-      jpfPass.setBounds(140,30,300,20);
+        jtfUser.setBounds(140, 10, 300, 20);
+        jpfPass.setBounds(140, 30, 300, 20);
 
-      btnLogin.setBounds(140,55,100,30);
-      btnReset.setBounds(240,55,100,30);
-      btnExit.setBounds(340,55,100,30);
+        btnLogin.setBounds(140, 55, 100, 30);
+        btnReset.setBounds(240, 55, 100, 30);
+        btnExit.setBounds(340, 55, 100, 30);
 
-      this.add(jlabUser);
-      this.add(jlabPass);
-      this.add(jpfPass);
-      this.add(btnLogin);
-      this.add(btnReset);
-      this.add(btnExit);
-
-      this.setSize(500,300);
-      this.setVisible(true);
     }
 
-  public void actionPerformed(ActionEvent ae)
-  {
+    public void actionPerformed(ActionEvent ae) {
+        if(btnReset.isEnabled() == true){
+            jtfUser.setText("");
+            jpfPass.setText("");
+        }
+    }
 
-  }
+    public static void main(String args[]) {
+        LoginScreen ls = new LoginScreen("Snake&LadderLogin");
 
-  public static void main(String args[])
-  {
-    LoginScreen ls = new LoginScreen("Snake&LadderLogin");
-
-  }
-
-
+    }
 
 
 }
