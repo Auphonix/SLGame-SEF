ddc4d7f352b434211e80882ce0350689c8244885
Chizzele <s3395415@student.rmit.edu.au>
2016-04-29 08:52:27 +1000

    Added files via upload

--- src/Game/LoginScreen.java
+++ src/Game/LoginScreen.java
@@ -1,71 +1,70 @@
-package Game;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-class LoginScreen extends JFrame implements ActionListener {
-    private String username;
-    private String password;
-    private int index;
+class LoginScreen extends JFrame implements ActionListener
+{
+  JButton btnLogin, btnReset, btnExit;
+  JTextField jtfUser;
+  JPasswordField jpfPass;
+  JLabel jlabUser, jlabPass, jlabLoginPress;
 
-    JButton btnLogin, btnReset, btnExit;
-    JTextField jtfUser;
-    JPasswordField jpfPass;
-    JLabel jlabUser, jlabPass;
+    public LoginScreen(String name)
+    {
+      super(name);
+      btnLogin = new JButton("Login");
+      btnReset = new JButton("Register");
+      btnExit = new JButton("Exit");
 
+      jtfUser = new JTextField();
+      jpfPass = new JPasswordField();
 
+      jlabUser = new JLabel("Username:");
+      jlabPass = new JLabel("Password:");
+      jlabLoginPress = new JLabel("fw");
 
-    public LoginScreen() {
+      this.setLayout(null);
+      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JFrame loginScreen = new JFrame("Login Screen");  //creates the frame and sets properties
-        loginScreen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        loginScreen.setSize(500, 300);
-        loginScreen.setVisible(true);
+      btnLogin.addActionListener(this);
+      btnReset.addActionListener(this);
+      btnExit.addActionListener(this);
 
-        JTextField jtfUser = new JTextField("Please Enter Username"); //Creates an instance of all elements
-        JPasswordField jpfPass = new JPasswordField();
-        JLabel jlabUser = new JLabel("Username:");
-        JLabel jlabPass = new JLabel("Password:");
-        JButton btnLogin = new JButton("Login");
-        JButton btnReset = new JButton("Reset");
-        JButton btnExit = new JButton("Exit");
+      jlabUser.setBounds(10,10,120,20);
+      jlabPass.setBounds(10,30,120,10);
 
-        btnLogin.addActionListener(this); //Adds listeners on all buttons
-        btnReset.addActionListener(this);
-        btnExit.addActionListener(this);
+      jtfUser.setBounds(140,10,300,20);
+      jpfPass.setBounds(140,30,300,20);
 
-        jlabUser.setBounds(10, 10, 120, 20); //Adds positioning and sizes to all elements
-        jlabPass.setBounds(10, 30, 120, 20);
+      btnLogin.setBounds(140,55,100,30);
+      btnReset.setBounds(240,55,100,30);
+      btnExit.setBounds(340,55,100,30);
 
-        jtfUser.setBounds(140, 10, 300, 20);
-        jpfPass.setBounds(140, 30, 300, 20);
+      /*jlabLoginPress.setBounds(140,60,300,30);*/
 
-        btnLogin.setBounds(140, 55, 100, 30);
-        btnReset.setBounds(240, 55, 100, 30);
-        btnExit.setBounds(340, 55, 100, 30);
+      this.add(jlabUser);
+      this.add(jlabPass);
+      this.add(jpfPass);
+      this.add(btnLogin);
+      this.add(btnReset);
+      this.add(btnExit);
 
-        loginScreen.add(jlabUser); //Adds all instances to the JFrame
-        loginScreen.add(jlabPass);
-        loginScreen.add(jpfPass);
-        loginScreen.add(jtfUser);
-        loginScreen.add(btnLogin);
-        loginScreen.add(btnReset);
-        loginScreen.add(btnExit);
+      this.setSize(500,300);
+      this.setVisible(true);
     }
 
-    public void userSearch(String username, String password){
-    }
+  public void actionPerformed(ActionEvent ae)
+  {
+    jlabLoginPress.setIcon(new ImageIcon("/Users/mac/Documents/Atom/Java/SEF/Files/sample.png"));
+  }
 
-    public void actionPerformed(ActionEvent ae) {
-        if(btnReset.isEnabled() == true){ //Checks if btnReset is pressed
-            jtfUser.setText("");
-            jpfPass.setText("");
-        }
-        else if(btnExit.isEnabled() == true){ //Used to quit application
-        }
-        else if(btnLogin.isEnabled() == true){ //Used to login
-            super.dispose();
-        }
-    }
+  public static void main(String args[])
+  {
+    LoginScreen ls = new LoginScreen("Snake&LadderLogin");
+
+  }
+
+
+
+
 }
